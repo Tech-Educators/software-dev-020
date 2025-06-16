6abca7ba0fa548890cd7660e549c6b0a975d8789
Tim Smith <tim.smith2814@gmail.com>
2025-06-16 10:08:27 +0100

    Vite demos

---
+++ demos/week04/02-manual-installation-vite-demo/app.js
@@ -0,0 +1,7 @@
+console.log("Hello world!");
+document.body.style.backgroundColor = "tomato";
+document.body.style.color = "white";
+document.body.style.fontFamily = "monospace";
+
+const myReallyInneficientVariableName = "Tim";
+console.log(myReallyInneficientVariableName);
